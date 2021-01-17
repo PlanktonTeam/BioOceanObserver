135096872b1e508bd63f66a8fb5aece2afaa3b9f
jaseeverett <Jason.Everett@unsw.edu.au>
2021-01-18 08:59:06 +1100

    Add help and sharing menus

--- ui.R
+++ ui.R
@@ -48,5 +48,22 @@ navbarPage(title = div(img(src = "logo.png", style="margin-top: -10px; padding-r
                     tabsetPanel(
                       tabPanel("Section 1"),
                       tabPanel("Section 2"),
-                      tabPanel("Section 3")))
+                      tabPanel("Section 3"))),
+           navbarMenu("", icon = icon("question-circle"),
+                      tabPanel("Help", icon = icon("question")),
+                      tabPanel(actionLink("help_keyboard", "Keyboard shortcuts", icon = icon("keyboard-o"))),
+                      tabPanel("About", uiOutput("help_about"), icon = icon("info")),
+                      tabPanel(tags$a("", href = "https://github.com/jaseeverett/IMOS_BioOceanObserver/wiki", target = "_blank",
+                                      list(icon("globe"), "Documentation"))),
+                      tabPanel(tags$a("", href = "https://github.com/jaseeverett/IMOS_BioOceanObserver/issues", target = "_blank",
+                                      list(icon("github"), "Report issue")))),
+           navbarMenu("", icon = icon("share-alt"),
+                      tabPanel(tags$a("", href = "https://twitter.com/intent/tweet?url=https%3A%2F%2Fjaseeverett.shinyapps.io%2FIMOS_BioOceanObserver&text=Check%20out%20the%20fantastic%20new%20IMOS%20Biological%20Ocean%20Obervatory%20being%20developed", target = "_blank",
+                                      list(icon("twitter"), "Twitter"))),
+                      tabPanel(tags$a("", href = "https://www.facebook.com.sharer/sharer.php?u=https%3A%2F%2Fjaseeverett.shinyapps.io%2FIMOS_BioOceanObserver", target = "_blank",
+                                      list(icon("facebook"), "Facebook"))),
+                      tabPanel(tags$a("", href = "http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fjaseeverett.shinyapps.io%2FIMOS_BioOceanObserver", target = "_blank",
+                                      list(icon("pinterest-p"), "Pinterest"))),
+                      tabPanel(tags$a("", href = "http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fjaseeverett.shinyapps.io%2FIMOS_BioOceanObserver", target = "_blank",
+                                      list(icon("linkedin"), "LinkedIn"))))
 )
