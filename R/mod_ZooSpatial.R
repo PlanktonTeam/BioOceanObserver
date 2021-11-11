3710ab01829d523cfef5146a5ac8e5ed153ad9fc
clairedavies <21113590+clairedavies@users.noreply.github.com>
2021-11-12 10:37:52 +1100

    adding plots for sti and daynight

--- R/mod_ZooSpatial.R
+++ R/mod_ZooSpatial.R
@@ -13,25 +13,36 @@ mod_ZooSpatial_ui <- function(id){
   
   tagList(
     sidebarPanel(
-      # Species selector
-      selectizeInput(inputId = nsZooSpatial('species'), label = "Select a species", choices = unique(fMapData$Taxon), 
+      conditionalPanel(
+        condition="input.NRSspat == 1",  
+        # Species selector
+      selectizeInput(inputId = nsZooSpatial('species'), label = "Select a zooplankton species", choices = unique(fMapData$Taxon), 
                      selected = "Acartia danae")
+      ),
+      conditionalPanel(
+        condition="input.NRSspat == 2",  
+        # Species selector
+        selectizeInput(inputId = nsZooSpatial('species1'), label = "Select a copepod species", choices = unique(sti$Species), 
+                       selected = "Acartia danae")
+      )
     ),
     mainPanel(
       tabsetPanel(id = "NRSspat",
-        tabPanel("Observation maps",
+        tabPanel("Observation maps", value = 1, 
                  h6(textOutput(nsZooSpatial("DistMapExp"), container = span)),
                  plotOutput(nsZooSpatial("plot2"), height = 800) %>% shinycssloaders::withSpinner(color="#0dc5c1")
                  ),
-        tabPanel("Species Distribution maps",
+        tabPanel("Species Distribution maps", value = 2, 
                  h6(textOutput(nsZooSpatial("SDMsMapExp"), container = span)),
                  plotOutput(nsZooSpatial("SDMs"), height = 700) %>% shinycssloaders::withSpinner(color="#0dc5c1")
         ),
-        tabPanel("Species Temperature Index graphs",
-                 h6(textOutput(nsZooSpatial("STIsExp"), container = span))
+        tabPanel("Species Temperature Index graphs", value = 2, 
+                 h6(textOutput(nsZooSpatial("STIsExp"), container = span)),
+                 plotOutput(nsZooSpatial("STIs"), height = 700) %>% shinycssloaders::withSpinner(color="#0dc5c1")
         ),
-        tabPanel("Species Diurnal Behviour",
-                 h6(textOutput(nsZooSpatial("SDBsExp"), container = span))
+        tabPanel("Species Diurnal Behviour", value = 2, 
+                 h6(textOutput(nsZooSpatial("SDBsExp"), container = span)),
+                 plotOutput(nsZooSpatial("DNs"), height = 700) %>% shinycssloaders::withSpinner(color="#0dc5c1")
         )
       )
     )
@@ -77,7 +88,7 @@ mod_ZooSpatial_server <- function(id){
       
       # select initial map  ------------------------------------------------------------------------------
       
-    # Create plot object the plotOutput function is expecting
+    # Create dot map of distribution
     output$plot2 <- renderPlot({
     
               plot2 <- planktonr::pr_plot_fmap(selectedZS())
@@ -88,7 +99,7 @@ mod_ZooSpatial_server <- function(id){
     # add SDM if it is available
     output$SDMs <- renderImage({
 
-      speciesName <- stringr::str_replace_all(input$species, " ", "")
+      speciesName <- stringr::str_replace_all(input$species1, " ", "")
       filename <- paste("inst/app/www/SDMTweGAM_", speciesName, ".png", sep = "")
 
       list(src = filename,
@@ -97,8 +108,57 @@ mod_ZooSpatial_server <- function(id){
 
     }, deleteFile = FALSE)
 
+    # STI plot -----------------------------------------------------------------------------------------
+    # Subset data
     
-    # speciesName <- stringr::str_replace_all(Species, " ", "")
+    selectedSTI <- reactive({
+      
+      req(input$species1)
+      validate(need(!is.na(input$species1), "Error: Please select a species"))
+      
+      selectedSTI <- sti %>% 
+        dplyr::filter(.data$Species %in% input$species1) 
+      
+    }) %>% bindCache(input$species1)
+    
+    # sti plot
+    output$STIs <- renderPlot({
+      
+      validate(
+        need(nrow(selectedSTI()) > 20, "Not enough data for this copepod species")
+      )
+      
+      plotsti <- planktonr::pr_plot_sti(selectedSTI())
+      plotsti
+
+    }) %>% bindCache(input$species1)
+    
+    # daynight plot -----------------------------------------------------------------------------------------
+    # Subset data
+    
+    selecteddn <- reactive({
+      
+      req(input$species1)
+      validate(need(!is.na(input$species1), "Error: Please select a species"))
+      
+      selecteddn <- daynight %>% 
+        dplyr::filter(.data$Species %in% input$species1) 
+      
+    }) %>% bindCache(input$species1)
+    
+    # sti plot
+    output$DNs <- renderPlot({
+
+      validate(
+        need(length(unique(selecteddn()$daynight)) == 2 | nrow(selecteddn()) > 20, "Not enough data for this copepod species")
+      )
+      
+      plotdn <- planktonr::pr_plot_daynight(selecteddn())
+      plotdn
+      
+    }) %>% bindCache(input$species1)
+
+        # speciesName <- stringr::str_replace_all(Species, " ", "")
     # filename <- paste("inst/app/www/SDMTweGAM_", speciesName, ".png", sep = "")
     # img <- tryCatch(png::readPNG(filename), error = function(e){})
     # dft <-  data.frame(x=c(1,1,1,1), y=c(0,2,1,3), label = c('','No species distribution','map available',''))
