8d6b066fe178414c3f7023e7ddb35e929481fdf8
Jason Everett <jaseeverett@gmail.com>
2022-03-09 10:55:16 +1100

    Update rda to latest R version

--- data-raw/All_RData.R
+++ data-raw/All_RData.R
@@ -1,7 +1,7 @@
 ## script for all RData 
 ## Aim to change all source calls to planktonr functions
 
-library(planktonr)
+# library(planktonr)
 
 # NRS time series data
 datNRSz <- planktonr::pr_get_tsdata("NRS", "Z")
@@ -31,10 +31,11 @@ fMapDatap <- planktonr::pr_get_fMap_data("P")
 stiz <- planktonr::pr_get_sti("Z")
 stip <- planktonr::pr_get_sti("P")
 
-# daynight data (from CPR only)
+# Day-Night data (from CPR only)
 daynightz <- planktonr::pr_get_daynight("Z")
 daynightp <- planktonr::pr_get_daynight("P")
 
+library(magrittr)
 # Policy data set
 pr_get_pol <- function(Survey = 'NRS'){
   if(Survey == 'CPR'){
@@ -47,14 +48,14 @@ pr_get_pol <- function(Survey = 'NRS'){
   if(Survey == 'CPR') {
     
       Pol <-  Polr %>% 
-        dplyr::select(SampleDateUTC, Year, Month, BioRegion, Biomass_mgm3, PhytoBiomassCarbon_pgm3, ShannonCopepodDiversity, 
-                      ShannonPhytoDiversity) %>%
+        dplyr::select(SampleDateUTC, Year, Month, BioRegion, Biomass_mgm3, PhytoBiomassCarbon_pgm3, ShannonCopepodDiversityCPR, 
+                      ShannonPhytoDiversitycpr) %>%
         dplyr::filter(!BioRegion %in% c("North", "North-west")) %>%
         tidyr::pivot_longer(-c(SampleDateUTC:BioRegion), values_to = 'Values', names_to = "parameters")
 
       means <- Polr %>%
-        dplyr::select(BioRegion, Biomass_mgm3, PhytoBiomassCarbon_pgm3, ShannonCopepodDiversity, 
-                      ShannonPhytoDiversity) %>%
+        dplyr::select(BioRegion, Biomass_mgm3, PhytoBiomassCarbon_pgm3, ShannonCopepodDiversityCPR, 
+                      ShannonPhytoDiversitycpr) %>%
         tidyr::pivot_longer(-c(BioRegion), values_to = 'Values', names_to = "parameters") %>%
         dplyr::group_by(BioRegion, parameters) %>%
         dplyr::summarise(means = mean(Values, na.rm = TRUE), 
@@ -67,13 +68,13 @@ pr_get_pol <- function(Survey = 'NRS'){
     } else {
       
       Pol <-  Polr %>% 
-        dplyr::select(SampleDateLocal, Year, Month, StationName, StationCode, Biomass_mgm3, PhytoBiomassCarbon_pgL, Temperature_degC, ShannonCopepodDiversity, 
-                      ShannonPhytoDiversity, Salinity_psu, Chla_mgm3) %>%
+        dplyr::select(SampleDateLocal, Year, Month, StationName, StationCode, Biomass_mgm3, PhytoBiomassCarbon_pgL, CTDTemperature_degC, ShannonCopepodDiversity, 
+                      ShannonPhytoDiversity, CTDSalinity_psu, PigmentChla_mgm3) %>%
         tidyr::pivot_longer(-c(SampleDateLocal:StationCode), values_to = 'Values', names_to = "parameters")
       
       means <- Polr %>%
-        dplyr::select(StationName, Biomass_mgm3, PhytoBiomassCarbon_pgL, Temperature_degC, ShannonCopepodDiversity, 
-                      ShannonPhytoDiversity, Salinity_psu, Chla_mgm3) %>%
+        dplyr::select(StationName, Biomass_mgm3, PhytoBiomassCarbon_pgL, CTDTemperature_degC, ShannonCopepodDiversity, 
+                      ShannonPhytoDiversity, Salinity_psu, PigmentChla_mgm3) %>%
         tidyr::pivot_longer(-c(StationName), values_to = 'Values', names_to = "parameters") %>%
         dplyr::group_by(StationName, parameters) %>%
         dplyr::summarise(means = mean(Values, na.rm = TRUE), 
@@ -110,7 +111,6 @@ pr_get_polInfo <- function(Survey = 'NRS'){
       CPRinfo <- planktonr::pr_get_CPRTrips() %>%
         dplyr::mutate(Latitude = STARTLATITUDE, 
                       Longitude = ENDLONGITUDE) %>%
-        dplyr::rename(Region = REGION) %>%
         planktonr::pr_add_bioregions() %>%
         dplyr::select(BioRegion, STARTSAMPLEDATEUTC, MILES) %>%
         dplyr::mutate(Features = dplyr::case_when(BioRegion %in% c("South-east") ~ "narrow shelf intensifying currents, eddies and upwellings with low nutrient and primary productivity",
@@ -121,14 +121,15 @@ pr_get_polInfo <- function(Survey = 'NRS'){
  
 }
 
+
 PolNRS <- pr_get_pol("NRS")
 PolCPR <- pr_get_pol("CPR")
 NRSinfo <- pr_get_polInfo("NRS")
 CPRinfo <- pr_get_polInfo("CPR")
 
 ## microbial data
-library(tidyverse)
-datNRSm <- readr::read_csv("data/datNRSm.csv") %>%
+
+datNRSm <- readr::read_csv("data-raw/datNRSm.csv") %>%
   dplyr::mutate(SampleDepth_m = as.numeric(stringr::str_sub(TripCode_depth, -3, -1))) %>%
   dplyr::select(StationName, SampleDepth_m, StationCode, SampleDateLocal, Year, Month, 
          Prochlorococcus_cells_ml:Eukaryote_Chlorophyll_Index) %>%
