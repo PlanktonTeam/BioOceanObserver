275a4b83f9390347d995f0f5eff159c72d84e016
Jason Everett <jaseeverett@gmail.com>
2022-07-13 14:17:52 +1000

    Increase use of functions

--- R/mod_PhytoTsCPR.R
+++ R/mod_PhytoTsCPR.R
@@ -2,7 +2,7 @@
 #'
 #' @description A shiny Module.
 #'
-#' @param id,input,output,session Internal parameters for {shiny}.
+#' @param id,input,output,session Internal Parameters for {shiny}.
 #'
 #' @noRd 
 #'
@@ -11,45 +11,23 @@ mod_PhytoTsCPR_ui <- function(id){
   nsPhytoTsCPR <- NS(id)
   tagList(
     sidebarLayout(
-      sidebarPanel(
-        conditionalPanel(
-          condition="input.CPRpts == 1",
-          checkboxInput(inputId = nsPhytoTsCPR("scaler"), label = strong("Change the plot scale to log10"), value = FALSE),
-          selectInput(inputId = nsPhytoTsCPR("parameter"), label = 'Select a parameter', choices = planktonr::pr_relabel(unique(datCPRp$parameters), style = "simple"), 
-                      selected = "PhytoAbund_Cellsm3")
-        ),
-        conditionalPanel(
-          condition="input.CPRpts == 2",
-          checkboxInput(inputId = nsPhytoTsCPR("scaler1"), label = strong("Change the plot scale to percent"), value = FALSE)
-        ),
-        absolutePanel(
-          plotOutput(nsPhytoTsCPR("plotmap")),
-          h6("Note there is very little data in the North and North-west regions"),
-          checkboxGroupInput(inputId = nsPhytoTsCPR("region"), label = "Select a region", choices = unique(sort(datCPRp$BioRegion)), selected = unique(datCPRp$BioRegion)),
-          sliderInput(nsPhytoTsCPR("DatesSlide"), "Dates:", min = as.POSIXct('2009-01-01 00:00',
-                                                                             format = "%Y-%m-%d %H:%M",
-                                                                             tz = "Australia/Hobart"), max = Sys.time(), 
-                      value = c(as.POSIXct('2009-01-01 00:00',
-                                           format = "%Y-%m-%d %H:%M",
-                                           tz = "Australia/Hobart"), Sys.time()-1), timeFormat="%Y-%m-%d"),
-          downloadButton(nsPhytoTsCPR("downloadData"), "Data"),
-          downloadButton(nsPhytoTsCPR("downloadPlot"), "Plot"),
-          downloadButton(nsPhytoTsCPR("downloadNote"), "Notebook")
-        )
-     ),
+      fPlanktonSidebar(id = id, panel_id = "CPRpts", input = input, dat = datCPRp),
       mainPanel(
         tabsetPanel(id = "CPRpts",
                     tabPanel("Trend Analysis", value = 1,
                              h6(textOutput(nsPhytoTsCPR("PlotExp1"), container = span)),  
-                             plotOutput(nsPhytoTsCPR("timeseries1")) %>% shinycssloaders::withSpinner(color="#0dc5c1")
+                             plotOutput(nsPhytoTsCPR("timeseries1")) %>% 
+                               shinycssloaders::withSpinner(color="#0dc5c1")
                     ),
                     tabPanel("Climatologies", value = 1,
                              h6(textOutput(nsPhytoTsCPR("PlotExp2"), container = span)),  
-                             plotOutput(nsPhytoTsCPR("timeseries2"), height = 800) %>% shinycssloaders::withSpinner(color="#0dc5c1")
+                             plotOutput(nsPhytoTsCPR("timeseries2"), height = 800) %>% 
+                               shinycssloaders::withSpinner(color="#0dc5c1")
                     ),
                     tabPanel("Functional groups", value = 2,
                              h6(textOutput(nsPhytoTsCPR("PlotExp3"), container = span)),  
-                             plotOutput(nsPhytoTsCPR("timeseries3")) %>% shinycssloaders::withSpinner(color="#0dc5c1")
+                             plotOutput(nsPhytoTsCPR("timeseries3")) %>% 
+                               shinycssloaders::withSpinner(color="#0dc5c1")
                     )
         )
       )
@@ -73,7 +51,7 @@ mod_PhytoTsCPR_server <- function(id){
       selectedData <- datCPRp %>% 
         mutate(BioRegion = factor(.data$BioRegion, levels = c("Coral Sea", "Temperate East", "South-west", "South-east"))) %>%
         dplyr::filter(.data$BioRegion %in% input$region,
-                      .data$parameters %in% input$parameter,
+                      .data$Parameters %in% input$parameter,
                       dplyr::between(.data$SampleTime_Local, input$DatesSlide[1], input$DatesSlide[2])) %>%
         droplevels()
       
@@ -86,7 +64,7 @@ mod_PhytoTsCPR_server <- function(id){
       PhytoTsCPRMonthisNumeric = {class(selectedData()$Month_Local)},
       PhytoTsCPRDateisDate = {class(selectedData()$SampleTime_Local)},
       PhytoTsCPRRegionisFactor = {class(selectedData()$BioRegion)},
-      PhytoTsCPRparametersisChr = {class(selectedData()$parameters)},
+      PhytoTsCPRParametersisChr = {class(selectedData()$Parameters)},
       PhytoTsCPRValuesisNumeric = {class(selectedData()$Values)}
     )
     
@@ -96,10 +74,10 @@ mod_PhytoTsCPR_server <- function(id){
     
     # add text information 
     output$PlotExp1 <- renderText({
-      "A plot of selected Phytoplantkon parameters from the CPR around Australia, as a time series and a monthly climatology across bioregions. "
+      "A plot of selected Phytoplantkon Parameters from the CPR around Australia, as a time series and a monthly climatology across bioregions. "
     }) 
     output$PlotExp2 <- renderText({
-      "A plot of selected Phytoplantkon parameters from the CPR around Australia, as a time series, a monthly climatology and an annual mean for each bioregion"
+      "A plot of selected Phytoplantkon Parameters from the CPR around Australia, as a time series, a monthly climatology and an annual mean for each bioregion"
     }) 
     output$PlotExp3 <- renderText({
       "A plot of functional groups from the light microscope phytoplankton counts from the CPR around Australia, as a time series and a monthly climatology for each bioregion"
@@ -107,13 +85,10 @@ mod_PhytoTsCPR_server <- function(id){
     
     
     # Plot Trends -------------------------------------------------------------
-
-      ts1 <- reactive({
-        if(input$scaler){
-        trans <- "log10"
-      } else {
-        trans <- "identity"
-      }
+    
+    ts1 <- reactive({
+      
+      trans <- dplyr::if_else(input$scaler1, "log10", "identity")
       
       p1 <- planktonr::pr_plot_Trends(selectedData(), Trend = "Raw", Survey = "CPR", method = "lm", trans = trans)
       p2 <- planktonr::pr_plot_Trends(selectedData(), Trend = "Month", Survey = "CPR", method = "loess", trans = trans) + 
@@ -127,34 +102,31 @@ mod_PhytoTsCPR_server <- function(id){
       ts1()
     }, height = function() {length(unique(selectedData()$BioRegion)) * 200}) 
     
-      
+    
     # Climatologies -----------------------------------------------------------
     
     output$timeseries2 <- renderPlot({
-      if(input$scaler){
-        trans <- "log10"
-      } else {
-        trans <- "identity"
-      }
+     
+      trans <- dplyr::if_else(input$scaler1, "log10", "identity")
       
       if (identical(input$region, "")) return(NULL)
       if (identical(input$parameter, "")) return(NULL)
       
       p1 <- planktonr::pr_plot_TimeSeries(selectedData(), Survey = "CPR", trans = trans) + 
-        ggplot2::theme(legend.position = 'none',
+        ggplot2::theme(legend.position = "none",
                        axis.title.y = ggplot2::element_blank())
       
       p2 <- planktonr::pr_plot_Climatology(selectedData(), Survey = "CPR", Trend = "Month", trans = trans) + 
-        ggplot2::theme(legend.position = 'bottom',
+        ggplot2::theme(legend.position = "bottom",
                        axis.title.y = ggplot2::element_blank())
       
       p3 <- planktonr::pr_plot_Climatology(selectedData(), Survey = "CPR", Trend = "Year", trans = trans) + 
         ggplot2::theme(axis.title.y = ggplot2::element_blank(),
-                       legend.position = 'bottom')
+                       legend.position = "bottom")
       
-      titleplot <- names(planktonr::pr_relabel(input$parameter, style = 'simple'))
+      titleplot <- names(planktonr::pr_relabel(input$parameter, style = "simple"))
       
-      p1 / (p2 | p3) + patchwork::plot_layout(guides = 'collect') + patchwork::plot_annotation(
+      p1 / (p2 | p3) + patchwork::plot_layout(guides = "collect") + patchwork::plot_annotation(
         title = titleplot)
       
       
@@ -179,7 +151,7 @@ mod_PhytoTsCPR_server <- function(id){
       PhytoFGCPRMonthisNumeric = {class(selectedDataFG()$Month_Local)},
       PhytoFGCPRDateisDate = {class(selectedDataFG()$SampleTime_Local)},
       PhytoFGCPRRegionisFactor = {class(selectedDataFG()$BioRegion)},
-      PhytoFGCPRparametersisChr = {class(selectedDataFG()$parameters)},
+      PhytoFGCPRParametersisChr = {class(selectedDataFG()$Parameters)},
       PhytoFGCPRValuesisNumeric = {class(selectedDataFG()$Values)}
     )
     
@@ -189,11 +161,7 @@ mod_PhytoTsCPR_server <- function(id){
         return(NULL)
       }
       
-      if(input$scaler1){
-        scale <- "Percent"
-      } else {
-        scale <- "Actual"
-      }
+      scale <- dplyr::if_else(input$scaler3, "Actual", "Percent")
       
       p1 <- planktonr::pr_plot_tsfg(selectedDataFG(), Scale = scale)
       p2 <- planktonr::pr_plot_tsfg(selectedDataFG(), Scale = scale, Trend = "Month")
@@ -206,28 +174,11 @@ mod_PhytoTsCPR_server <- function(id){
       ts3()
     }, height = function() {length(unique(selectedDataFG()$BioRegion)) * 200})     
     
-    
-    # Downloads ---------------------------------------------------------------
-    # Table of selected dataset ----
-    output$table <- renderTable({
-      # datasetInput()
-    })
-    
-    #Downloadable csv of selected dataset ----
-    output$downloadData <- downloadHandler(
-      filename = function() {
-        paste0(tools::file_path_sans_ext(input$ycol),"_", format(Sys.time(), "%Y%m%dT%H%M%S"), ".csv")
-      },
-      content = function(file) {
-        vroom::vroom_write(selectedData(), file, delim = ",")
-      })
+    # Download -------------------------------------------------------
+    # Downloadable csv of selected dataset ----
+    output$downloadData <- fDownloadDataServer(input, selectedData())
     
     # Download figure
-    # output$downloadPlot <- downloadHandler(
-    #   filename = function() {paste(input$parameter, '.png', sep='') },
-    #   content = function(file) {
-    #     ggsave(file, plot = plotInput(), device = "png")
-    #   }
-    # )
+    output$downloadPlot <- fDownloadPlotServer(input, ts3())
   })
 }
--- R/mod_PolLTM.R
+++ R/mod_PolLTM.R
@@ -2,7 +2,7 @@
 #'
 #' @description A shiny Module.
 #'
-#' @param id,input,output,session Internal parameters for {shiny}.
+#' @param id,input,output,session Internal Parameters for {shiny}.
 #'
 #' @noRd 
 #'
@@ -16,9 +16,8 @@ mod_PolLTM_ui <- function(id){
         shinydashboard::menuSubItem(text = "Find out more about EOVs here", href = "https://www.goosocean.org/index.php?option=com_content&view=article&layout=edit&id=283&Itemid=441"),
         plotOutput(nsPolLTM("plotmap")),
         radioButtons(inputId = nsPolLTM("SiteLTM"), label = "Select a station", choices = unique(sort(LTnuts$StationName)), selected = "Port Hacking"),
-        downloadButton(nsPolLTM("downloadData"), "Data"),
-        downloadButton(nsPolLTM("downloadPlot"), "Plot"),
-        downloadButton(nsPolLTM("downloadNote"), "Notebook")
+        fDownloadData(id, "Data"),
+        fDownloadPlot(id, "Plot")
       ),
       mainPanel(id = "EOV paramters from Long Term Monitoring", 
                 h6(textOutput(nsPolLTM("PlotExp1"), container = span)),
@@ -45,7 +44,7 @@ mod_PolLTM_server <- function(id){
       selectedDataLTM <- PolLTM %>% 
         dplyr::filter(.data$StationName %in% input$SiteLTM,
                       .data$SampleDepth_m < 15,
-                      !.data$parameters %in% c("Ammonium_umolL","Nitrite_umolL", "Oxygen_umolL")) 
+                      !.data$Parameters %in% c("Ammonium_umolL","Nitrite_umolL", "Oxygen_umolL")) 
       
     }) %>% bindCache(input$SiteLTM)
     
@@ -62,7 +61,7 @@ mod_PolLTM_server <- function(id){
       PolLTMProjectisChr = {class(selectedDataLTM()$Project)},
       PolLTMStationisChr = {class(selectedDataLTM()$StationName)},
       PolLTMCodeisChr = {class(selectedDataLTM()$StationCode)},
-      PolLTMparametersisChr = {class(selectedDataLTM()$parameters)},
+      PolLTMParametersisChr = {class(selectedDataLTM()$Parameters)},
       PolLTMValuesisNumeric = {class(selectedDataLTM()$Values)}
     )
     
@@ -71,8 +70,8 @@ mod_PolLTM_server <- function(id){
     }) %>% bindCache(input$SiteLTM)
     
     info <- reactive({
-      info <- outputs() %>% dplyr::select(.data$slope, .data$p, .data$parameters) %>% unique() %>%
-        dplyr::arrange(.data$parameters)
+      info <- outputs() %>% dplyr::select(.data$slope, .data$p, .data$Parameters) %>% unique() %>%
+        dplyr::arrange(.data$Parameters)
     }) %>% bindCache(input$SiteLTM)
     
     stationData <- reactive({
