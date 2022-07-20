1c6eca36b22af6b56a7a18065c7ca4a59d81f35d
Jason Everett <jaseeverett@gmail.com>
2022-07-20 14:05:40 +1000

    Fix checks

--- R/app_ui.R
+++ R/app_ui.R
@@ -30,8 +30,8 @@ app_ui <- function(request) {
                             We aim to enhance the availability and understanding of biological data and make it 
                             accessible to broader and non-specialist audiences in order to accelerate the next 
                             generation of scientific insights."),
-                          p("Data was sourced from Australia’s ",tags$a(href="https://imos.org.au","Integrated Marine Observing System (IMOS)"),
-                            " – IMOS is enabled by the National Collaborative Research Infrastructure Strategy (NCRIS). 
+                          p("Data was sourced from Australia's ",tags$a(href="https://imos.org.au","Integrated Marine Observing System (IMOS)"),
+                            " - IMOS is enabled by the National Collaborative Research Infrastructure Strategy (NCRIS). 
                             It is operated by a consortium of institutions as an unincorporated joint venture, with 
                             the University of Tasmania as Lead Agent."),
                           h3("Code"),
