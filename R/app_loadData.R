687c39abd5c7f9a92d3c5fb0c38bca0d3a8d01df
ordiology <ordiology@gmail.com>
2022-10-27 14:06:06 +1100

    Removed app_loadData. Including the load data in the app.R file instead.
    This should allow the build to happen because the sysdata exists in the app but it should then be overwritten.

--- R/app_loadData.R
+++
@@ -1,9 +0,0 @@
-# Load .rda file if it exists and use it in the application instead of default data
-datapath <- "./data/sysdata.rda"
-if (file.exists(datapath)) {
-  print(paste0("Loading data from ", datapath))
-  # load(datapath)
-  file.copy(datapath, "./R/sysdata.rda", overwrite = T)
-}
-
-
