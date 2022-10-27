fbbe407d0e8d6a87573c90952c110a8821990b99
ordiology <ordiology@gmail.com>
2022-10-27 13:00:44 +1100

    Testing copying the sysdata.rda file to the R directory when app starts.

--- R/app_loadData.R
+++ R/app_loadData.R
@@ -2,7 +2,8 @@
 datapath <- "./data/sysdata.rda"
 if (file.exists(datapath)) {
   print(paste0("Loading data from ", datapath))
-  load(datapath)
+  # load(datapath)
+  file.copy(datapath, "./R/sysdata.rda", overwrite = T)
 }
 
 
