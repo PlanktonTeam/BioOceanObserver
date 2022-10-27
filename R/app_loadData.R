07ca4c744f5a9e59cb704973e315e61e1f57fe37
ordiology <ordiology@gmail.com>
2022-10-27 12:53:00 +1100

    Last version worked. Trying again without having to explicitely apply usethis::use_data()

--- R/app_loadData.R
+++ R/app_loadData.R
@@ -3,15 +3,6 @@ datapath <- "./data/sysdata.rda"
 if (file.exists(datapath)) {
   print(paste0("Loading data from ", datapath))
   load(datapath)
-  usethis::use_data(Nuts, Pigs, Pico, LTnuts, 
-                    fMapDataz, fMapDatap, legendPlot,
-                    MooringTS, MooringClim,
-                    PolNRS, PolCPR, PolLTM, NRSinfo, CPRinfo,
-                    datCPRz, datCPRp, datCPRw,
-                    datNRSz, datNRSp, datNRSm, datNRSw,
-                    NRSfgz, NRSfgp, CPRfgz, CPRfgp, PMapData,
-                    stiz, stip, daynightz, daynightp, PMapData2,
-                    overwrite = TRUE, internal = TRUE)
 }
 
 
