2a61d69422893cb6545dcb46494f06a4f15715ef
clairedavies <21113590+clairedavies@users.noreply.github.com>
2022-12-05 16:30:52 +1100

    Update mod_MoorBGC.R

--- R/mod_MoorBGC.R
+++ R/mod_MoorBGC.R
@@ -40,7 +40,7 @@ mod_MoorBGC_server <- function(id){
         df <- data.frame(SampleDate = seq.Date(to = lubridate::ceiling_date(Sys.Date(), "year"),
                                                from = lubridate::ceiling_date(Sys.Date() - lubridate::years(noYear), "year"),
                                                by = "day")) %>% 
-              dplyr::mutate(TIME = lubridate::yday(.data$SampleDate), 
+              dplyr::mutate(TIME = lubridate::yday(.data$SampleDate) + 10956, 
                             year = lubridate::year(.data$SampleDate)) %>% 
               dplyr::inner_join(df %>% dplyr::filter(StationName %in% Station), by = 'TIME') %>%
               unique()
@@ -79,8 +79,8 @@ mod_MoorBGC_server <- function(id){
       df <- data.frame(SampleDate = seq.Date(to = lubridate::ceiling_date(Sys.Date(), "year"),
                                                   from = lubridate::ceiling_date(Sys.Date() - lubridate::years(noYear), "year"),
                                                   by = "day")) %>% 
-        dplyr::mutate(DOY = lubridate::yday(.data$SampleDate)) %>% 
-        dplyr::inner_join(MooringTS %>% dplyr::filter(StationName %in% Station), by = 'DOY') %>%
+        dplyr::mutate(DOY = lubridate::yday(.data$SampleDate) + 10956) %>% 
+        dplyr::inner_join(df %>% dplyr::filter(StationName %in% Station), by = 'DOY') %>%
         dplyr::select(-c(.data$DOY)) %>%
         tidyr::pivot_wider(c(.data$SampleDate, .data$StationName, .data$StationCode), names_from = 'Names', values_from = 'CLIM') 
     }
