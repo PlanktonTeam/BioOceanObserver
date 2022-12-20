b0dd41da7a600c1df84cbdc31969969d88e1e8eb
ordiology <ordiology@gmail.com>
2022-12-20 19:04:37 +1100

    Moved package environment variable assignment outside data upload expressions. Testing again with filtered sysdata.

--- R/zzz.R
+++ R/zzz.R
@@ -24,9 +24,6 @@ pkg.env <- new.env(parent = emptyenv())
     load(tmp)
     cat(file=stderr(), "Up-to-date data accessed from opendap.\n")
     pkg.env$new.data <- TRUE
-    for (currVar in data.vars) {
-      pkg.env[[currVar]] <- get(currVar)
-    }
   }, error = function(e) { 
     cat(file=stderr(), as.character(e))
     tryCatch({
@@ -36,22 +33,22 @@ pkg.env <- new.env(parent = emptyenv())
       dap.data <- jsonlite::fromJSON(rawToChar(httr::GET(dap.url)$content))
       file.req <- dap.data$file$filename 
       tmp <- tempfile(fileext='.rda')   
-      httr::GET(dap.data$file$link$href[[which(dap.data$file$filename == "sysdata.rda")]], httr::write_disk(tmp))
+      httr::GET(dap.data$file$link$href[[which(dap.data$file$filename == "sysdata1.rda")]], httr::write_disk(tmp))
       load(tmp)
       cat(file=stderr(), "Up-to-date data accessed from dap.\n")
       pkg.env$new.data <- TRUE
-      for (currVar in data.vars) {
-        pkg.env[[currVar]] <- get(currVar)
-      }
+ 
     }, error = function(e) { 
       # Warn that data is not up-to-date
       cat(file=stderr(), as.character(e))
       cat(file=stderr(), "Building the imosboo package using built in sysdata.rda. If this message appears when running the app, the data being served is not up-to-date.\n")
       pkg.env$new.data <- FALSE
-      for (currVar in data.vars) {
-        pkg.env[[currVar]] <- get(currVar)
-      }
     }) 
-  }) 
+  })
+  
+  # Add data vars to package environment variable
+  for (currVar in data.vars) {
+    pkg.env[[currVar]] <- get(currVar)
+  }
 }
 
