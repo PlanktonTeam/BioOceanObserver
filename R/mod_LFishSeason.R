0bb64378a67c01fa3e1955bfc230ce89e7f226a5
Jason Everett <jaseeverett@gmail.com>
2023-02-02 14:17:40 +1100

    Fix typo

--- R/mod_LFishSeason.R
+++ R/mod_LFishSeason.R
@@ -34,7 +34,7 @@ mod_LFishSeason_ui <- function(id){
       shiny::fluidRow(
         shiny::column(width = 6,
                       style = "padding:0px; margin:0px;",
-                      shiny::h4("June = August"),
+                      shiny::h4("June - August"),
                       leaflet::leafletOutput(nsLFishSeason("LFMapWin"), width = "99%", height = "250px") %>% 
                         shinycssloaders::withSpinner(color="#0dc5c1")), 
         shiny::column(width = 6,
@@ -60,7 +60,7 @@ mod_LFishSeason_server <- function(id){
         dplyr::filter(.data$Species == input$species) %>%
         dplyr::distinct(.data$Latitude, .data$Longitude, .keep_all = TRUE) %>% 
         dplyr::mutate(Season = dplyr::case_when(.data$Month_Local >= 3 &.data$Month_Local <= 5 ~ "March - May",
-                                                .data$Month_Local >= 6 &.data$Month_Local <= 8 ~ "June = August",
+                                                .data$Month_Local >= 6 &.data$Month_Local <= 8 ~ "June - August",
                                                 .data$Month_Local >= 9 &.data$Month_Local <= 11 ~ "September - November",
                                                 TRUE ~ "December - February"))
       return(dat)
@@ -97,7 +97,7 @@ mod_LFishSeason_server <- function(id){
     observe({
       LeafletObs(sdf = LFDatar() %>% dplyr::filter(.data$Season == "December - February"), name = "LFMapSum", Type = 'PA')
       LeafletObs(sdf = LFDatar() %>% dplyr::filter(.data$Season == "September - November"), name = "LFMapAut", Type = 'PA')
-      LeafletObs(sdf = LFDatar() %>% dplyr::filter(.data$Season == "June = August"), name = "LFMapWin", Type = 'PA')
+      LeafletObs(sdf = LFDatar() %>% dplyr::filter(.data$Season == "June - August"), name = "LFMapWin", Type = 'PA')
       LeafletObs(sdf = LFDatar() %>% dplyr::filter(.data$Season == "March - May"), name = "LFMapSpr", Type = 'PA')
       
     })
