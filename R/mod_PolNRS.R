1d6ca31704a1c7bc2c12814cdb94c539fe70de45
Jason Everett <jaseeverett@gmail.com>
2023-02-03 12:48:57 +1100

    Change `EOV` to `EOVs`

--- R/mod_PolNRS.R
+++ R/mod_PolNRS.R
@@ -63,7 +63,9 @@ mod_PolNRS_server <- function(id){
     }) %>% bindCache(input$Site)
     
     info <- reactive({
-      info <- outputs() %>% dplyr::select(.data$slope, .data$p, .data$Parameters) %>% unique()
+      info <- outputs() %>% 
+        dplyr::select(.data$slope, .data$p, .data$Parameters) %>% 
+        unique()
     }) %>% bindCache(input$Site)
     
     stationData <- reactive({
@@ -119,26 +121,26 @@ mod_PolNRS_server <- function(id){
     
     gg_out1 <- reactive({
       
-      p1 <- planktonr::pr_plot_EOV(outputs(), EOV = "Biomass_mgm3", trans = "log10", col = pkg.env$col12[2], labels = "no") 
-      p2 <- planktonr::pr_plot_EOV(outputs(), EOV = "PhytoBiomassCarbon_pgL", trans = "log10", col = pkg.env$col12[4]) 
+      p1 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Biomass_mgm3", trans = "log10", col = pkg.env$col12[2], labels = "no") 
+      p2 <- planktonr::pr_plot_EOVs(outputs(), EOV = "PhytoBiomassCarbon_pgL", trans = "log10", col = pkg.env$col12[4]) 
       
-      p3 <- planktonr::pr_plot_EOV(outputs(), EOV = "ShannonCopepodDiversity", trans = "log10", col = pkg.env$col12[1], labels = "no") 
-      p4 <- planktonr::pr_plot_EOV(outputs(), EOV = "ShannonPhytoDiversity", trans = "log10", col = pkg.env$col12[3])
+      p3 <- planktonr::pr_plot_EOVs(outputs(), EOV = "ShannonCopepodDiversity", trans = "log10", col = pkg.env$col12[1], labels = "no") 
+      p4 <- planktonr::pr_plot_EOVs(outputs(), EOV = "ShannonPhytoDiversity", trans = "log10", col = pkg.env$col12[3])
       
-      p5 <- planktonr::pr_plot_EOV(outputs(), EOV = "CTDTemperature_degC", trans = "identity", col = pkg.env$col12[5], labels = "no")
-      p6 <- planktonr::pr_plot_EOV(outputs(), EOV = "PigmentChla_mgm3", trans = "log10", col = pkg.env$col12[3], labels = "no") 
-      p7 <- planktonr::pr_plot_EOV(outputs(), EOV = "CTDSalinity_PSU", trans = "identity", col = pkg.env$col12[7])
+      p5 <- planktonr::pr_plot_EOVs(outputs(), EOV = "CTDTemperature_degC", trans = "identity", col = pkg.env$col12[5], labels = "no")
+      p6 <- planktonr::pr_plot_EOVs(outputs(), EOV = "PigmentChla_mgm3", trans = "log10", col = pkg.env$col12[3], labels = "no") 
+      p7 <- planktonr::pr_plot_EOVs(outputs(), EOV = "CTDSalinity_PSU", trans = "identity", col = pkg.env$col12[7])
   
 
-      p8 <- planktonr::pr_plot_EOV(outputs(), EOV = "Ammonium_umolL", trans = "identity", col = pkg.env$col12[8], labels = "no")
-      p9 <- planktonr::pr_plot_EOV(outputs(), EOV = "Nitrate_umolL", trans = "identity", col = pkg.env$col12[9], labels = "no")
-      p10 <- planktonr::pr_plot_EOV(outputs(), EOV = "Silicate_umolL", trans = "identity", col = pkg.env$col12[10], labels = "no")
+      p8 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Ammonium_umolL", trans = "identity", col = pkg.env$col12[8], labels = "no")
+      p9 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Nitrate_umolL", trans = "identity", col = pkg.env$col12[9], labels = "no")
+      p10 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Silicate_umolL", trans = "identity", col = pkg.env$col12[10], labels = "no")
        
       if(input$Site %in% c('Maria Island', 'Rottnest Island')){
-        p11 <- planktonr::pr_plot_EOV(outputs(), EOV = "Phosphate_umolL", trans = "log10", col = pkg.env$col12[11], labels = "no")
-        p12 <- planktonr::pr_plot_EOV(outputs(), EOV = "Oxygen_umolL", trans = "identity", col = pkg.env$col12[12])
+        p11 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Phosphate_umolL", trans = "log10", col = pkg.env$col12[11], labels = "no")
+        p12 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Oxygen_umolL", trans = "identity", col = pkg.env$col12[12])
       } else {
-        p11 <- planktonr::pr_plot_EOV(outputs(), EOV = "Phosphate_umolL", trans = "log10", col = pkg.env$col12[11], labels = "yes")
+        p11 <- planktonr::pr_plot_EOVs(outputs(), EOV = "Phosphate_umolL", trans = "log10", col = pkg.env$col12[11], labels = "yes")
         p12 <- ggplot2::ggplot + ggplot2::geom_blank()
       }
       
