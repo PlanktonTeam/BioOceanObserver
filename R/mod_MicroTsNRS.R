e61ef30d88716a71bf93aa038a3ed0b9b45b4447
clairedavies <21113590+clairedavies@users.noreply.github.com>
2023-02-23 13:45:10 +1100

    making single plots the right size

--- R/mod_MicroTsNRS.R
+++ R/mod_MicroTsNRS.R
@@ -181,7 +181,10 @@ mod_MicroTsNRS_server <- function(id){
       
       output$timeseries3 <- renderPlot({
         gg_out3()
-      }, height = function() {length(unique(selectedDataDepth()$StationName)) * 200})
+      }, height = function() {
+        if(length(unique(selectedDataDepth()$StationName)) < 2) 
+        {300} else 
+        {length(unique(selectedDataDepth()$StationName)) * 200}})
       
       # Download -------------------------------------------------------
       output$downloadData3 <- fDownloadButtonServer(input, selectedData(), "Enviro") # Download csv of data
