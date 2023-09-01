4e35555de261ff3236deedc7b0c13f463dcc3ac7
ordiology <ordiology@gmail.com>
2023-09-01 18:38:10 +1000

    If a config.rda exists and contains the variable data.path that is a url string, the app will try to source data from this url. If it does not exist, the app will try to source data from the CSIRO DAP.

--- R/000.R
+++ R/000.R
@@ -20,18 +20,20 @@ pkg.env <- new.env(parent = emptyenv())
 
   tryCatch({
     # Access data from local server (fastest)
-    cat(file=stderr(), "Attempting to access data from opendap.\n")
-    opendap.url <- "https://data-cbr.it.csiro.au/files/sc-opendap-work/work/sc-artefact/imosboo/sysdata.rda"
+    # The variable data.url is a string that is the url to the served data and is defined in a local config.rda file.
+    # If config.rda doesn't exist, this step results in an error that is then handled.
+    cat(file=stderr(), "Attempting to access data from data.url declared in config.rda.\n")
+    load("config.rda")
     tmp <- tempfile(fileext='.rda')   
-    httr::GET(opendap.url, httr::write_disk(tmp))
+    httr::GET(data.url, httr::write_disk(tmp))
     load(tmp)
-    cat(file=stderr(), "Up-to-date data accessed from opendap.\n")
+    cat(file=stderr(), paste0("Up-to-date data accessed from ", data.url,".\n"))
     pkg.env$new.data <- TRUE
   }, error = function(e) { 
     cat(file=stderr(), as.character(e))
     tryCatch({
       # Access data from DAP (fallback)
-      cat(file=stderr(), "Attempting to access data from dap.\n")
+      cat(file=stderr(), "Attempting to access data from CSIRO DAP.\n")
       dap.url <- "https://data.csiro.au/dap/ws/v2/collections/csiro:54520/data"
       dap.data <- jsonlite::fromJSON(rawToChar(httr::GET(dap.url)$content))
       file.req <- dap.data$file$filename 
@@ -44,7 +46,7 @@ pkg.env <- new.env(parent = emptyenv())
     }, error = function(e) { 
       # Warn that data is not up-to-date
       cat(file=stderr(), as.character(e))
-      cat(file=stderr(), "Building the biooceanobserver package using built in sysdata.rda. If this message appears when running the app, the data being served is not up-to-date.\n")
+      cat(file=stderr(), "Building the Biological Ocean Observer package using built in sysdata.rda. If this message appears when running the app, the data being served is not up-to-date.\n")
       pkg.env$new.data <- FALSE
     }) 
   })
