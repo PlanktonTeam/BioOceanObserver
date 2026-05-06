e44a023020c4ffed489198a60740c614ff8b1252
clairedavies <21113590+clairedavies@users.noreply.github.com>
2026-05-07 08:09:30 +1000

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -92,8 +92,12 @@ golem_add_external_resources <- function(){
   )
   
   shiny::tags$head(
-    favicon(),
-    bundle_resources(
+    # script for accessning google analytics from csiro server
+    if( Sys.getenv('SHINY_PORT') != "" ) {
+      tags$head(includeHTML("GoogleAnalytics.html"))
+    },
+    
+    favicon(),    bundle_resources(
       path = app_sys('app/www'),
       app_title = 'Biological Ocean Observer'
     ),
