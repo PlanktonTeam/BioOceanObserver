ca56a849af96a0935e34de2849571673d2ebfe56
Claire Davies <21113590+clairedavies@users.noreply.github.com>
2026-06-16 16:21:42 +1000

    Remove hardcoded Mapbox public token

--- R/run_app.R
+++ R/run_app.R
@@ -12,7 +12,7 @@ run_app <- function(
   options = list(), 
   enableBookmarking = NULL,
   uiPattern = "/",
-  MAPBOX_PUBLIC_TOKEN="pk.eyJ1IjoiamFzZWV2ZXJldHQiLCJhIjoiY21tdHA1dDllMDJ3czJwcTdrcDU5OG93aiJ9.OO3DjqVgqcDL5Etpgk_qzg",
+  MAPBOX_PUBLIC_TOKEN="",
   ...
 ) {
   with_golem_options(
