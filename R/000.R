b4de9cd0dae05dcc30644d38add233d187fa73b1
Will Hore-Lacy <will.hore-lacy@csiro.au>
2026-08-26 11:54:11 +1000

    Setup data read from GitHub as an option.  Include testing code for read errors and read speed.

--- R/000.R
+++ R/000.R
@@ -4,6 +4,10 @@ pkg.env <- new.env(parent = emptyenv())
 .onLoad <- function(libname, pkgname){
   cat(file=stderr(), "New data load call.\n")
 
+  # Test for response to 'errors' in data read
+  # data_error_str <- Sys.getenv("DATA_ERROR") # Try with env. variables
+  # data_error_vec <- strsplit(data_error_str, ",")[[1]]
+  data_error_vec <- c() #c("bowen", "github", "dap")
 
   # Non-grep vars: LTnuts, datCPRw, PMapData
   data.vars <- c("Nuts", "Pigs", "Pico", "ctd", "CSChem",
@@ -31,33 +35,79 @@ pkg.env <- new.env(parent = emptyenv())
     # Access data from local server (fastest)
     # The variable data.url is a string that is the url to the served data and is defined in a local config.rda file.
     # If config.rda doesn't exist, this step results in an error that is then handled.
+    start_time <- Sys.time()
     cat(file=stderr(), "Attempting to access data from Bowen\n")
+    
+    if("bowen" %in% data_error_vec) {
+        stop("Force Bowen read error")
+    }
+    
     thredds_url <- "https://data-cbr.it.csiro.au/thredds/fileServer/catch_all/imosboo/BOODataUpload/sysdata.rda"
     tmp <- tempfile(fileext='.rda')
     httr::GET(thredds_url, httr::write_disk(tmp))
     load(tmp)
     cat(file=stderr(), paste0("Up-to-date data accessed from Bowen\n"))
     pkg.env$new.data <- TRUE
+    pkg.env$data.source <- "Bowen"
+    pkg.env$load.time <- Sys.time() - start_time
+    pkg.env$file.size <- file.size(tmp)
   }, error = function(e) {
     cat(file=stderr(), as.character(e))
     tryCatch({
-      # Access data from DAP (fallback)
-      cat(file=stderr(), "Attempting to access data from CSIRO DAP.\n")
-      dap.url <- "https://data.csiro.au/dap/ws/v2/collections/csiro:54520/data"
-      dap.data <- jsonlite::fromJSON(rawToChar(httr::GET(dap.url)$content))
-      file.req <- dap.data$file$filename
+      # Access from GitHub
+      start_time <- Sys.time()
+      cat(file=stderr(), "Attempting to access data from GitHub\n")
+      
+      if("github" %in% data_error_vec) {
+        stop("Force GitHub read error")
+      }
+      
+      github_url <- "https://github.com/PlanktonTeam/BioOceanObserver/raw/refs/heads/main/R/sysdata.rda"
       tmp <- tempfile(fileext='.rda')
-      httr::GET(dap.data$file$link$href[[which(dap.data$file$filename == "sysdata.rda")]], httr::write_disk(tmp))
+      httr::GET(github_url, httr::write_disk(tmp))
       load(tmp)
-      cat(file=stderr(), "Up-to-date data accessed from dap.\n")
+      cat(file=stderr(), paste0("Up-to-date data accessed from GitHub\n"))
       pkg.env$new.data <- TRUE
-
+      pkg.env$data.source <- "GitHub"
+      pkg.env$load.time <- Sys.time() - start_time
+      pkg.env$file.size <- file.size(tmp)
+      
     }, error = function(e) {
-      # Warn that data is not up-to-date
       cat(file=stderr(), as.character(e))
-      cat(file=stderr(), "Building the Biological Ocean Observer package using built in sysdata.rda. If this message appears when running the app, the data being served is not up-to-date.\n")
-      pkg.env$new.data <- FALSE
-     })
+      tryCatch({
+        # Access data from DAP (fallback)
+        start_time <- Sys.time()
+        cat(file=stderr(), "Attempting to access data from CSIRO DAP.\n")
+        
+        if("dap" %in% data_error_vec) {
+          stop("Force DAP read error")
+        }
+        
+        dap.url <- "https://data.csiro.au/dap/ws/v2/collections/csiro:54520/data"
+        dap.data <- jsonlite::fromJSON(rawToChar(httr::GET(dap.url)$content))
+        file.req <- dap.data$file$filename
+        tmp <- tempfile(fileext='.rda')
+        httr::GET(dap.data$file$link$href[[which(dap.data$file$filename == "sysdata.rda")]], httr::write_disk(tmp))
+        load(tmp)
+        cat(file=stderr(), "Up-to-date data accessed from dap.\n")
+        pkg.env$new.data <- TRUE
+        
+        pkg.env$data.source <- "DAP"
+        pkg.env$load.time <- Sys.time() - start_time
+        pkg.env$file.size <- file.size(tmp)
+        
+      }, error = function(e) {
+        # Warn that data is not up-to-date
+        cat(file=stderr(), as.character(e))
+        cat(file=stderr(), "Building the Biological Ocean Observer package using built in sysdata.rda. If this message appears when running the app, the data being served is not up-to-date.\n")
+        pkg.env$new.data <- FALSE
+        
+        pkg.env$data.source <- "Package"
+        pkg.env$load.time <- 0
+        pkg.env$file.size <- 0
+      })
+    })
+    
    })
 
   # Add data vars to package environment variable
